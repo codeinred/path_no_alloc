fe6ceda9d91bd21b869542fa5988758537043807
Alecto Irene Perez <perez.cs@pm.me>
2022-11-25 12:27:30 -0700

    Remove string validation outside loop

--- benches/main.rs
+++ benches/main.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId};
 use path_no_alloc::with_paths;
@@ -17,13 +17,16 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     let p1 = "Call me Ishmael. Some years ago - never mind how long precisely - having little or no money in my purse, and nothing particular to interest me on shore";
     let p2 = "I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation.";
 
+    let p1_slices = array_from_idx::<129, _>(|i| &p1[..i]);
+    let p2_slices = array_from_idx::<129, _>(|i| &p2[..i]);
+
     let mut group = c.benchmark_group("Path join");
 
 
     for i in 1..128 {
         group.bench_with_input(BenchmarkId::new("with_paths!", i), &i, |b, i| b.iter(|| {
-            let p1 = &p1[..*i];
-            let p2 = &p2[..*i];
+            let p1 = p1_slices[*i];
+            let p2 = p2_slices[*i];
 
             with_paths! {
                 path = p1 / p2 => black_box(path)
@@ -31,8 +34,8 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         }));
 
         group.bench_with_input(BenchmarkId::new("Path.join", i), &i, |b, i| b.iter(|| {
-            let p1 = &p1[..*i];
-            let p2 = &p2[..*i];
+            let p1 = p1_slices[*i];
+            let p2 = p2_slices[*i];
 
             black_box(Path::new(p1).join(p2))
         }));
